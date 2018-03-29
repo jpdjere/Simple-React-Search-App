67ad6f12a07af6d31d36f363c3bc9a597fa6d6a6
Juan Pablo Djeredjian <jpdjeredjian@gmail.com>
2018-03-29 12:40:48 -0300

    Initial config

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+import ReactDOM from "react-dom"
+
+class App extends Component{
+  render(){
+    <hi>Hi!</hi>
+  }
+}
+
+let root = document.getElementById("root");
+ReactDOM.render(<App />,root)
